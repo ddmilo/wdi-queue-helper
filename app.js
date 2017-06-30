dba72ef8f87c6742645b410b866d2b94dd4e326b
ddmilo <ddmilo1029@gmail.com>
2017-06-30 11:16:58 -0400

    blah

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '--hbs');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -42,5 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000 | console.log('mic check'));
 
 module.exports = app;
